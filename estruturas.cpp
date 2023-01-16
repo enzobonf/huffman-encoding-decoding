148eb8acf1bc8e2d4d7a44683b5b444f6071f49b
enzobonf <enzobonfx@gmail.com>
2023-01-16 17:07:27 -0300

    feat: geração da árvore de caracteres

--- estruturas.cpp
+++ estruturas.cpp
@@ -16,6 +16,13 @@ struct No {
         this->freq = freq;
         this->esq = this->dir = nullptr;
     }
+
+    No(char ch, int freq, No* esq, No* dir){
+        this->ch = ch;
+        this->freq = freq;
+        this->esq = esq;
+        this->dir = dir;
+    }
 };
 
 struct Compare {
@@ -26,9 +33,23 @@ struct Compare {
 
 void construirArvoreHuffman(string text){
     unordered_map<char, int> freq;
-    for(char ch : text){
+    for(char ch : text){ // O(tamanho do texto em caracteres)
         freq[ch]++;
     }
 
-    cout << freq['a'] << endl;
+    priority_queue<No*, vector<No*>, Compare> fila;
+
+    for(auto pair : freq){ // O(n de caracteres diferentes no texto)
+        // first = caractere | second = frequencia
+        fila.push(new No(pair.first, pair.second));
+    }
+
+    while(fila.size() != 1){
+        No *esq = fila.top(); fila.pop();
+        No *dir = fila.top(); fila.pop();
+        int soma = esq->freq + dir->freq;
+        cout << "soma: " << soma << endl;
+        fila.push(new No('\0', soma, esq, dir));
+    }
+
 }
